37e7e4f817527b6ff7a16b5617573754f2193afd
Shubham Krishna <shubhamkrishnasound@gmail.com>
2022-05-26 02:10:36 +0530

    fixed tempreture control app

--- temperature-control-app/src/App.js
+++ temperature-control-app/src/App.js
@@ -1,10 +1,29 @@
 import "./styles.css";
-
+import React, { useState } from "react";
 export default function App() {
+  const [value, setValue] = useState(14);
+  const [color, setColor] = useState("netural");
+  function inc() {
+    if (value === 30) return;
+    setValue(value + 1);
+    if (value >= 14) setColor("hot");
+  }
+  function dec() {
+    if (value === 0) return;
+    setValue(value - 1);
+    if (value <= 14) setColor("cold");
+  }
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <div class="background">
+        <div class="output-container">
+          <div class={`output ${color}`}>
+            <span>{value} C</span>
+          </div>
+        </div>
+        <button onClick={inc}>+</button>
+        <button onClick={dec}>-</button>
+      </div>
     </div>
   );
 }

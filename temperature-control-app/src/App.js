44f25aefb7f5da6eda180d0ef585199edbe7bfc3
Shubham Krishna <shubhamkrishnasound@gmail.com>
2022-05-26 18:32:45 +0530

    add use effect

--- temperature-control-app/src/App.js
+++ temperature-control-app/src/App.js
@@ -1,23 +1,28 @@
 import "./styles.css";
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 export default function App() {
+
+  useEffect(()=>{
+    if (value >= 14) setColor("hot");
+    if (value < 14) setColor("cold");
+  },[value])
   const [value, setValue] = useState(14);
   const [color, setColor] = useState("netural");
   function inc() {
     if (value === 30) return;
     setValue(value + 1);
-    if (value >= 14) setColor("hot");
+   
   }
   function dec() {
     if (value === 0) return;
     setValue(value - 1);
-    if (value <= 14) setColor("cold");
+   
   }
   return (
     <div className="App">
-      <div class="background">
-        <div class="output-container">
-          <div class={`output ${color}`}>
+      <div className="background">
+        <div className="output-container">
+          <div className={`output ${color}`}>
             <span>{value} C</span>
           </div>
         </div>
